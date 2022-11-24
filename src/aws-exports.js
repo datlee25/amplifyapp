09dc7d0f612ba3af366b3cac3219fdfa19ae0d16
Lê Thành Đạt <datlt@timebird.org>
2022-11-24 11:43:03 +0700

    interact graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -26,7 +26,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://5oxvuvggo5bqbk2kbbueyjqrxi.appsync-api.ap-southeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-southeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-23fdqtwks5btxbtk4wfkaitt54"
 };
 
 
