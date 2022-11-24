ec24c8e218ef56ec6bb0f5c674dd01f7dfa25171
Lê Thành Đạt <datlt@timebird.org>
2022-11-24 15:08:27 +0700

    Added graphql and storage

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -30,7 +30,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://5oxvuvggo5bqbk2kbbueyjqrxi.appsync-api.ap-southeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-23fdqtwks5btxbtk4wfkaitt54"
+    "aws_appsync_apiKey": "da2-23fdqtwks5btxbtk4wfkaitt54",
+    "aws_user_files_s3_bucket": "shulee-image-storage40605-staging",
+    "aws_user_files_s3_bucket_region": "ap-southeast-1"
 };
 
 
